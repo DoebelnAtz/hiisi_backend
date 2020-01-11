dd85786871dd4c95a13cb8183067421431ac8776
Doebelnatz <axel.ej.adlercreutz@gmail.com>
2020-01-12 01:59:45 +0200

    password hashing implemented

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,5 +1,5 @@
 const { validationResult } = require('express-validator');
-
+const bcrypt = require('bcryptjs');
 const HttpError = require('../models/http-error');
 const User = require('../models/user');
 
@@ -13,27 +13,37 @@ const getUsers = async (req, res, next) => {
     res.json({users: users.map(user => user.toObject({ getters: true }))});
 };
 
-const signUp = async (req, res, next) => {
+let signUp;
+signUp = async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return next(new HttpError('Invalid credentials' + errors, 400));
     }
-    const { username, password, intraId } = req.body;
+    const {username, password, intraId} = req.body;
 
     let existingUser;
     try {
-        existingUser = await User.findOne({ username: username })
+        existingUser = await User.findOne({username: username})
     } catch (e) {
         return next(new HttpError('Signup failed, please try again later'), 500);
     }
 
     if (existingUser) {
-        return next(new HttpError('User already exists, please login instead', 400 ));
+        return next(new HttpError('User already exists, please login instead', 400));
+    }
+
+    let hashedPassword;
+    try {
+        hashedPassword = bcrypt.hash(password, 10);
+    } catch (e) {
+        return next(
+            new HttpError('Cannot create user', 500)
+        );
     }
 
     const createdUser = new User({
         username,
-        password,
+        password: hashedPassword,
         intraId,
         blogPosts: []
     });
@@ -43,7 +53,7 @@ const signUp = async (req, res, next) => {
     } catch (e) {
         return next(new HttpError('Signup failed, please try again later'), 500);
     }
-    res.status(201).json({user: createdUser.toObject({ getters: true })});
+    res.status(201).json({user: createdUser.toObject({getters: true})});
 };
 
 const login = async (req, res, next) => {
@@ -61,9 +71,20 @@ const login = async (req, res, next) => {
         return next(new HttpError('login failed, please try again'), 500)
     }
 
-    if (!existingUser || existingUser.password !== password) {
+    let isValidPass = false;
+    try {
+        isValidPass = await bcrypt.compare(password, existingUser.password)
+    } catch (e) {
+        return next(
+            new HttpError('login failed, please try again', 500)
+        );
+    }
+
+    if (!existingUser || !isValidPass) {
         return next(new HttpError('Invalid credentials, please try again'), 401);
     }
+
+
 
     res.json({ message: 'logged in!' })
 };
