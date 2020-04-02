00910dbd1d542bf94c0f7ab574d0d4e62dc1c4a8
Doebelnatz <axel.ej.adlercreutz@gmail.com>
2020-04-02 13:58:20 +0300

    queries.js refactor

---
+++ src/postgres/queries.ts
@@ -0,0 +1,40 @@
+const pg = require('pg');
+const logger = require('../logger');
+
+const pool = new pg.Pool({
+	user: 'admin',
+	host: '207.154.211.76',
+	database: 'hivemind',
+	password: process.env.db,
+});
+
+const client = new pg.Client({
+	user: 'admin',
+	host: '207.154.211.76',
+	database: 'hivemind',
+	password: process.env.db,
+});
+
+client.connect(function(err: Error) {
+	if (err) {
+		return console.error('could not connect to postgres', err);
+	}
+	client.query('SELECT NOW() AS "theTime"', function(
+		err: Error,
+		result: any,
+	) {
+		if (err) {
+			return console.error('error running query', err);
+		}
+		console.log(result.rows[0].theTime);
+		// >> output: 2018-08-23T14:02:57.117Z
+		client.end();
+	});
+});
+
+module.exports = {
+	query: (text: string, params: any[]) => {
+		return pool.query(text, params);
+	},
+	connect: () => pool.connect(),
+};
